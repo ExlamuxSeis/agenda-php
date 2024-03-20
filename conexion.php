218212caa1b9fe2a1f493176d24c6c37e0b79d14
Exlamux6 <ti21_victor.ul@uttc.edu.mx>
2024-03-20 09:22:09 -0600

    Agrega conexion a DB

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@
         public static function dbconexion(){
             // Creando un objeto para la conexión de la base de datos
             try {
-                return $conn = new PDO('mysql:host=localhost;dbname=agenda2', 'root', '');
+                return $conn = new PDO('mysql:host=localhost;dbname=agenda2', 'root', 'asdfg12345');
                 // echo 'conexión establecida';
             } catch (PDOException $error) {
                 die($error->getMessage());
